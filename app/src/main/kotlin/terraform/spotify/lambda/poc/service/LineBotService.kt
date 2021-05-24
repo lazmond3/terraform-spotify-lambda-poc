fe2011269866ae7bea2872ed5a1750055aced16b
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 09:44:23 +0900

    [java] add line bot service samples

--- app/src/main/kotlin/terraform/spotify/lambda/poc/service/LineBotService.kt
+++ app/src/main/kotlin/terraform/spotify/lambda/poc/service/LineBotService.kt
@@ -1,4 +1,26 @@
 package terraform.spotify.lambda.poc.service
 
-class LineBotService {
+import com.linecorp.bot.client.LineMessagingClient
+import com.linecorp.bot.model.PushMessage
+import com.linecorp.bot.model.ReplyMessage
+import com.linecorp.bot.model.message.TextMessage
+
+class LineBotService(
+    val token: String
+) {
+    val client = LineMessagingClient.builder(token).build();
+
+    fun sendMessage(mid: String, text: String) = client.pushMessage(
+        PushMessage(
+            mid,
+            TextMessage(text)
+        )
+    ).get()
+
+    fun replyToMessage(replyToken: String, text: String) = client.replyMessage(
+        ReplyMessage(
+            replyToken,
+            TextMessage(text)
+        )
+    ).get()
 }
