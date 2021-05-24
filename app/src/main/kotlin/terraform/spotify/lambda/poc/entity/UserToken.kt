a9fd75d6953ede1856218f6b43c1531d49cdc8a4
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 09:43:49 +0900

    [java] ADD userToken これnullable性が気になる。

--- app/src/main/kotlin/terraform/spotify/lambda/poc/entity/UserToken.kt
+++ app/src/main/kotlin/terraform/spotify/lambda/poc/entity/UserToken.kt
@@ -1,13 +1,21 @@
 package terraform.spotify.lambda.poc.entity
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import terraform.spotify.lambda.poc.annotation.NoArgsConstructor
-import java.time.LocalDateTime
 
 @NoArgsConstructor
 data class UserToken(
     val userId: String? = null,
     val refreshToken: String? = null,
     val accessToken: String? = null,
-    val expiresIn: Int? = null,
+    val expiresAt: Int? = null,
     val updatedAt: String? = null,
-)
+) {
+    constructor(result: Map<String, AttributeValue>) : this(
+        userId = result.get("UserId")?.s,
+        refreshToken = result.get("RefreshToken")?.s,
+        accessToken = result.get("AccessToken")?.s,
+        expiresAt = result.get("ExpiresAt")?.n?.toInt(),
+        updatedAt = result.get("UpdatedAt")?.s
+    )
+}
