e029f53482d83f30f438941c80102c1ed5983711
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 09:41:20 +0900

    [java] [LambdaHandlerTest] mid の削除？

--- app/src/test/kotlin/terraform/spotify/lambda/poc/handler/LambdaHandlerTest.kt
+++ app/src/test/kotlin/terraform/spotify/lambda/poc/handler/LambdaHandlerTest.kt
@@ -1,5 +1,11 @@
 package terraform.spotify.lambda.poc.handler
 
+import com.linecorp.bot.client.LineMessagingClient
+import com.linecorp.bot.client.LineMessagingClientBuilder
+import com.linecorp.bot.model.PushMessage
+import com.linecorp.bot.model.message.Message
+import com.linecorp.bot.model.message.TemplateMessage
+import com.linecorp.bot.model.message.TextMessage
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
@@ -16,5 +22,25 @@ internal class LambdaHandlerTest {
             jpCal.toInstant(), ZoneId.of("Asia/Tokyo")
         )
         println(now.format(DateTimeFormatter.ISO_DATE_TIME))
+    }
+
+    val channelToken =
+        System.getenv("LINE_BOT_CHANNEL_ACCESS_TOKEN")
+
+    @Test
+    fun linebotTest() {
+        // secret
+        val client = LineMessagingClient.builder(channelToken).build();
+        val message = TextMessage("hello world!")
+        val sentResult = client.pushMessage(
+            PushMessage(
+                "",
+                message
+
+            )
+        ).get()
+
+        println("hell world, result: $sentResult")
+
     }
 }
