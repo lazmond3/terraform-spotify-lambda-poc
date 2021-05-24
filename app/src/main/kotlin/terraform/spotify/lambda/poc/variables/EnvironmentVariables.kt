f78c963bac2fdea960c473dd56705f89d65904cf
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 10:44:52 +0900

    [java] delete spotify アクセストークンを削除した

--- app/src/main/kotlin/terraform/spotify/lambda/poc/variables/EnvironmentVariables.kt
+++ app/src/main/kotlin/terraform/spotify/lambda/poc/variables/EnvironmentVariables.kt
@@ -3,8 +3,6 @@ package terraform.spotify.lambda.poc.variables
 import terraform.spotify.lambda.poc.exception.SystemException
 
 class EnvironmentVariables {
-    val spotifyRefreshToken = System.getenv("SPOTIFY_REFRESH_TOKEN")
-        ?: throw SystemException("SPOTIFY_REFRESH_TOKEN is not set")
     val spotifyClientId = System.getenv("SPOTIFY_CLIENT_ID")
         ?: throw SystemException("SPOTIFY_CLIENT_ID is not set")
     val spotifyClientSecret = System.getenv("SPOTIFY_CLIENT_SECRET")
