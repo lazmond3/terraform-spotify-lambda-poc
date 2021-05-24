a889249e7673067f6a71d04e8623d5da4ada96b1
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 09:45:22 +0900

    [java] WIP controller

--- app/src/main/kotlin/terraform/spotify/lambda/poc/controller/LineBotHookController.kt
+++ app/src/main/kotlin/terraform/spotify/lambda/poc/controller/LineBotHookController.kt
@@ -1,4 +1,15 @@
 package terraform.spotify.lambda.poc.controller
 
-class LineBotHookController {
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import terraform.spotify.lambda.poc.service.LineBotService
+
+class LineBotHookController(val token: String) {
+    val lineBotService = LineBotService(token)
+
+    fun handle(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+
+    }
+
 }
