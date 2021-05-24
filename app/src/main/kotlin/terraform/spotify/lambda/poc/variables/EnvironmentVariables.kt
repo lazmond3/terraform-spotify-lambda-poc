df952cecca2bd472d984480b5172d96090099607
lazmond3 <lazmond3@users.noreply.github.com>
2021-05-24 09:44:07 +0900

    [java] environment: add system line bot channel token

--- app/src/main/kotlin/terraform/spotify/lambda/poc/variables/EnvironmentVariables.kt
+++ app/src/main/kotlin/terraform/spotify/lambda/poc/variables/EnvironmentVariables.kt
@@ -9,4 +9,6 @@ class EnvironmentVariables {
         ?: throw SystemException("SPOTIFY_CLIENT_ID is not set")
     val spotifyClientSecret = System.getenv("SPOTIFY_CLIENT_SECRET")
         ?: throw SystemException("SPOTIFY_CLIENT_SECRET is not set")
+    val lineBotChannelAccessToken = System.getenv("LINE_BOT_CHANNEL_ACCESS_TOKEN")
+        ?: throw SystemException("LINE_BOT_CHANNEL_ACCESS_TOKEN is not set")
 }
