979a925245c2078e128c53299866daba6c91487f
lazmond3 <lazmond3@users.noreply.github.com>
2021-06-30 11:35:11 +0900

    [terraform] aws.virginia in aws_acm_certificate_validation

--- terraform/cert-route.tf
+++ terraform/cert-route.tf
@@ -59,6 +59,7 @@ resource "aws_route53_record" "validations" {
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.validations : record.fqdn]
+  provider                = "aws.virginia"
 }
 
 
