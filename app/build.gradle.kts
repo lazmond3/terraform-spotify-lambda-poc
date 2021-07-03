1afe58a11f6be6e4cbbe768548656b812c186847
lazmond3 <lazmond3@users.noreply.github.com>
2021-07-04 04:50:00 +0900

    [gradle] テスト突破, gradle で バージョンを2.12.3 に設定

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,17 @@ dependencies {
     implementation("com.amazonaws:aws-java-sdk-dynamodb")
 
     // retrofit
+//    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+//    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+//    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.0")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.0")
+//    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+//    implementation("com.squareup.okhttp3:mockwebserver:4.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
+
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.squareup.okhttp3:mockwebserver:4.9.0")
 
